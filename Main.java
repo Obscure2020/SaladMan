11acfa7b23175e85a68355d9af5bb95cbf8d29e4
Joey Sodergren <jas.elgoog@gmail.com>
2023-02-04 12:07:11 -0500

    Splitting Bitstream

--- Main.java
+++ Main.java
@@ -2,13 +2,25 @@ import java.math.BigInteger;
 import java.util.*;
 
 class Main {
+    private static String[] trimChars(String input, int num){
+        String[] arr = new String[2];
+        arr[0] = input.substring(0,num);
+        arr[1] = input.substring(num);
+        return arr;
+    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.print("Hexadecimal Data: ");
         String input = scan.nextLine();
         scan.close();
 
-        BigInteger data = new BigInteger(input, 16);
-        System.out.println("Base 10: " + data.toString(10));
+        String binData = new BigInteger(input, 16).toString(2);
+        String[] b = trimChars(binData, 8);
+        System.out.println(b[0]);
+        b = trimChars(b[1], 1);
+        System.out.println(b[0]);
+        b = trimChars(b[1],3);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
     }
 }
