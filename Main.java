aa7e12d5514cbe9802811c0eef5f8cf1217d239d
Joey Sodergren <jas.elgoog@gmail.com>
2023-02-04 11:35:55 -0500

    Beginning Java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Hexadecimal Data: ");
+        String input = scan.nextLine();
+        scan.close();
+
+        System.out.println();
+        System.out.println("Fantastic!");
+        System.out.println("[" + input + "]");
+        System.out.println("I have NO idea what to do with this junk.");
+    }
+}
---
+++ Run.bat
@@ -0,0 +1,2 @@
+@echo off
+javac *.java && java Main && del /Q /S *.class 1>nul
