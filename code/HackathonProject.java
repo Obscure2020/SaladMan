7ae7b506c9e72135bc81c7893b4c8a8d281437ec
Liam Henninger <84351411+zeldafan664@users.noreply.github.com>
2023-02-04 13:49:06 -0500

    updated for tracking
    The code uses a HashMap to store the number of times each RFID label has been scanned. The loop iterates over the elements of the rfidVector, and for each element, the code checks if the label is already in the trace map. If the label is already in the map, the code retrieves the current count and increases it by one. If the label is not already in the map, the code adds the label with a count of 1. After the loop, the for loop is used to iterate over the keys in the trace map, and the contents of the map are written to the rfid.csv file.

--- code/HackathonProject.java
+++ code/HackathonProject.java
@@ -1,54 +1,48 @@
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Vector;
 import java.util.Scanner;
 
 public class HackathonProject {
+   public static Vector<String> scanRFID() {
+      Scanner scanner = new Scanner(System.in);
+      Vector<String> rfidVector = new Vector<String>();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int choice = 0;
+      while (true) {
+         System.out.println("RFID Scanner Input: ");
+         String rfidData = scanner.nextLine();
 
-        while (choice != 100) {
-            System.out.println("Menu: ");
-            System.out.println("Enter 1 to Read RFID Data");
-            System.out.println("Enter 100 to quit");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1: //Read RFID Data 
-                    Vector<String> rfidVector = readRFID();
-                    System.out.println("RFID Labels: ");
-                    for (int i = 0; i < rfidVector.size(); i++) {
-                        System.out.println(rfidVector.get(i));
-                    }
+         if (rfidData.equals("stop")) {
+            break;
+         }
+         System.out.println("RFID Data: " + rfidData);
+         rfidVector.add(rfidData);
+      }
+      return rfidVector;
+   }
 
-            }
-
-        }
-
-    }
-
-    public static Vector readRFID() {
-        Scanner scanner = new Scanner(System.in);
-        Vector<String> rfidVector = new Vector<String>();
-
-        while (true) {
-            System.out.println("Reading RFID Data...(Type 'stop' to stop)");
-            String rfidData = scanner.nextLine();
-
-            if (rfidData.equals("stop")) {
-                break;
-            }
-            System.out.println("RFID Data: " + rfidData);
-            rfidVector.add(rfidData);
-        }
-        System.out.println("RFID Labels:");
-        for (int i = 0; i < rfidVector.size(); i++) {
-            System.out.println(rfidVector.get(i));
-
-        }
-
-        return rfidVector;
-    }
-
+   public static void main(String[] args) {
+      Vector<String> rfidVector = scanRFID();
+      HashMap<String, Integer> trace = new HashMap<String, Integer>();
+      for (int i = 0; i < rfidVector.size(); i++) {
+         String rfidLabel = rfidVector.get(i);
+         if (trace.containsKey(rfidLabel)) {
+            int count = trace.get(rfidLabel);
+            trace.put(rfidLabel, count + 1);
+         } else {
+            trace.put(rfidLabel, 1);
+         }
+      }
+      try (BufferedWriter bw = new BufferedWriter(new FileWriter("rfid.csv"))) {
+         for (String rfidLabel : trace.keySet()) {
+            bw.write(rfidLabel + "," + trace.get(rfidLabel));
+            bw.newLine();
+         }
+         System.out.println("RFID data written to rfid.csv");
+      } catch (IOException e) {
+         System.out.println("Error writing to file: " + e.getMessage());
+      }
+   }
 }
